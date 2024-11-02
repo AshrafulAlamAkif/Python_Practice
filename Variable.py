b5e4f3f40e98d5b538174e2676f6206077ea5213
AshrafulAlamAkif <ashraful15-2225@diu.edu.bd>
2024-11-02 23:13:24 +0600

    Multi words variable name

---
+++ Variable.py
@@ -0,0 +1,11 @@
+#Camel case(Fist letter is Lower case)
+myVariableName = "ashraful"
+print(myVariableName)
+
+#Pascal case(Fist letter is Uppercase)
+MyVariableName = "ashraful"
+print(MyVariableName)
+
+#Snake case(Each word is separated by an underscore character)
+my_variable_name = "akif"
+print(my_variable_name)
