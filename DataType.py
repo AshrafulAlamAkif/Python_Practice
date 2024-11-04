8595676e22549a1e55e006eac06783d71f0ce92c
AshrafulAlamAkif <ashraful15-2225@diu.edu.bd>
2024-11-04 23:14:42 +0600

    print data type

---
+++ DataType.py
@@ -0,0 +1,15 @@
+a = 122
+print(a)
+print(type(a)) #integer type
+
+b = 1.2
+print(b)
+print(type(b)) # float type
+
+c = 2213j
+c1 = 12+23j
+c2 = -5j
+print(c)
+print(type(c)) # compex type
+print(type(c1))
+print(type(c2))
