cbd51fee4ffabb8892aff56a93af0e7a44baa9e8
AshrafulAlamAkif <ashraful15-2225@diu.edu.bd>
2024-11-05 23:02:37 +0600

    String uses differently

---
+++ String.py
@@ -0,0 +1,12 @@
+#Assign multiple string to a Variable by using Double quotes
+a = """This is Akif
+and i am a software engineer"""
+print(a)
+
+b = '''What's going on 
+wvwryone want to know that'''
+print(b)
+
+#to get length of a String
+x = "Hello, World!"
+print(len(x))
