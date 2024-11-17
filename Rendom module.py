b1fe24d82d04c94f6ef2c4feff28b5e5540f1b7c
AshrafulAlamAkif <ashraful15-2225@diu.edu.bd>
2024-11-17 22:40:50 +0600

    Random Number print and also print from ArrayList

---
+++ Rendom module.py
@@ -0,0 +1,15 @@
+import random
+
+print(random.randrange(5, 15)) 
+
+list = ["a","b","c","d","e","f","d","g","h"]
+
+#get random item from the list
+print(random.choice(list))
+
+#get random item from the list with array mode
+print(random.choices(list))
+
+#print shuffled list
+random.shuffle(list)
+print(list)
