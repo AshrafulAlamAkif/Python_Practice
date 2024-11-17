76f537ee79d272713bea0d5f08b881d27739a90d
AshrafulAlamAkif <ashraful15-2225@diu.edu.bd>
2024-11-18 00:07:04 +0600

    Global variables

--- Function.py
+++ Function.py
@@ -1,7 +1,7 @@
-#Function call
+#Function call and same value name
 name = "Akif"
 def greet():
     name = "akif"
-    print("My name is",name)
+    print("My name is",name) #print inside variable value
 greet()
-print(name,"is the Global name")
+print(name,"is the Global name") #outside
